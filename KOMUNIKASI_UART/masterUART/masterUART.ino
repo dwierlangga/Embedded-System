f38fda8a15d74594e96e73e96d209dacb025d6bd
Erlangga <101104676+dwierlangga@users.noreply.github.com>
2023-11-28 11:08:15 +0700

    Update masterUART.ino

--- KOMUNIKASI_UART/masterUART/masterUART.ino
+++ KOMUNIKASI_UART/masterUART/masterUART.ino
@@ -1,28 +1,54 @@
-void setup(){
-  Serial.begin(9600); //menginisialisasi komunikasi serial antara mikrokontroler
+#include<SoftwareSerial.h>
+
+SoftwareSerial dataSerial(12, 13);
+
+unsigned long previousMillis = 0;
+const long interval = 2000;
+
+String arrData[2];
+float temp, hum;
+
+void setup() {
+  Serial.begin(9600);
+  dataSerial.begin(9600);
 }
 
-void loop(){
-  String request = ""; //Menginisiasi string kosong
-  while(Serial.available()>0){ //looping akan terus berjalan ketika ada data yang masuk pada serial
-    request +=char(Serial.read()); //menambahkan karakter request kedalam string
+void loop() {
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis >= interval){
+    previousMillis = currentMillis;
+
+    String data = "";
+    while(dataSerial.available()>0){
+      data += char(dataSerial.read());
+    }
+    data.trim();
+
+    if(data != ""){
+      int index = 0;
+      for(int i=0; i<= data.length(); i++){
+        char delimiter = '#';
+        if(data[i] != delimiter){
+          arrData[index] += data[i];
+        }
+        else{
+          index++;
+        }
+      }
+
+      if(index == 1){
+        Serial.println("Temperature : " + arrData[0]);
+        Serial.println("Humidity    : " + arrData[1]);
+        Serial.println();
+      }
+
+      temp = arrData[0].toFloat();
+      hum  = arrData[1].toFloat();
+      
+      arrData[0] = "";
+      arrData[1] = "";
+    }
+
+    dataSerial.println("Yes");
   }
-  request.trim(); //menghapus spasi ekstra di awal dan di akhir string
-
-  if(request == "Yes"){ //jika nilai request terpenuhi, maka data akan diproses
-    SendData();
-  }
-  request = ""; //// Setelah memproses permintaan, mengosongkan request untuk permintaan selanjutnya
-  delay(500);
-}
-
-//membuat void untuk tugas
-void SendData(){
-  int data1 = 0;
-  int data2 = 2;
-  data1 = data1 + 1;
-  data2 = data2 + 2;
-
-  String DataSend = String(data1)+"#"+String(data2);membuat sebuah objek String yang menggabungkan dua variabel atau nilai, yaitu data1 dan data2, dipisahkan oleh tanda pagar (#).
-  Serial.println(DataSend); //mencetak data dari parameter string DataSend
 }
