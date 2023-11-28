11d7a0e5abd462b9336a6af4cbbd08d6bd5abdf6
Erlangga <101104676+dwierlangga@users.noreply.github.com>
2023-11-28 11:09:20 +0700

    Update slaveUART.ino

--- KOMUNIKASI_UART/slaveUART/slaveUART.ino
+++ KOMUNIKASI_UART/slaveUART/slaveUART.ino
@@ -1,34 +1,32 @@
-String arrData[2]; //mendefinisikan sebuah array yang berisi dua elemen bertipe data String
+#include <DHT.h>
+#define DHTPIN 9
+#define DHTTYPE DHT11
 
-int data11, data22; //mendefinisikan integer dengan parameter data11, data22
+DHT dht (DHTPIN, DHTTYPE);
 
 void setup(){
-  Serial.begin(9600); //menginisialisasi komunikasi serial antara mikrokontroler
+  Serial.begin(9600);
+  dht.begin();
 }
 
 void loop(){
-  String data = ""; //Menginisiasi string kosong
-  while(Serial.available()>0){ //looping akan terus berjalan ketika ada data yang masuk pada serial
-    data += char(Serial.read()); //menambahkan karakter request kedalam string
+  String request = "";
+  while(Serial.available()>0){
+    request +=char(Serial.read());
   }
-  data.trim(); //menghapus spasi ekstra di awal dan di akhir string
+  request.trim();
 
-  if(data != ""){ //memeriksa apakah variabel data tidak kosong
-    int index = 0;  // Deklarasi dan inisialisasi variabel index
-    for(int i=0; i<= data.length(); i++){ //mengulangi sebuah blok kode dengan variabel penghitung i dimulai dari 0 dan terus bertambah hingga i mencapai panjang string data.
-      char delimeter = '#'; //mendeklarasikan variabel bertipe data char yang disebut delimiter dan memberikan nilai karakter '#' ke dalam variabel tersebut.
-      if(data[i] != delimeter){ //memeriksa apakah karakter pada indeks i dalam string data tidak sama dengan karakter yang disimpan dalam variabel delimiter.
-        arrData[index] += data[i]; //digunakan untuk menambahkan karakter data[i] ke dalam elemen array arrData pada indeks index. Ini menggabungkan karakter data[i] dengan konten yang sudah ada di elemen array tersebut.
-      }
-      else{ //jika logika diatas tidak terpenuhi
-        index++; // Pindah ke elemen array berikutnya jika delimiter '#' ditemukan
-      }
-    }
-    if(index == 1){ //digunakan untuk memeriksa apakah nilai dari variabel index
-      Serial.println("Data 1 count : " + arrData[0]); //cetak data yang terbaca
-      Serial.println("Data 2 count : " + arrData[1]);
-    }
+  if(request == "Yes"){
+    sendData();
   }
-  Serial.println("yes"); //memberikan nilai request
-  delay(500);
+  request = "";
+  delay(1000);
+}
+
+void sendData(){
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+
+  String dataSend = String(t) + "#" + String(h);
+  Serial.println(dataSend);
 }
